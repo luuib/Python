9a67455107c10baed02e9573207c25656a235b60
luuib <129198298+luuib@users.noreply.github.com>
2024-02-15 10:43:49 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+#nome=input("Escreva o seu nome")
+#idade=input ("Escreva a sua idade")
+#idade=int(idade)
+#print(f"Olá, {nome}, voce tem {idade} anos")
+#try:
+ #   idade=int(input("Escreva a sua idade"))
+#except ValueError:
+ #       print("voce nao digitou a sua idade")
+ 
+ num1 = input ("escreva um numero")
+ 
+ num2= input("escreva um numero")
