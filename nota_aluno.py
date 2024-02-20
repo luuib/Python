56e30baff0fb568939f2711c4cc5876da4ce0d3f
luuib <129198298+luuib@users.noreply.github.com>
2024-02-20 10:40:06 -0300

    Add files via upload

---
+++ nota_aluno.py
@@ -0,0 +1,12 @@
+nota_aluno = float(input ("Escreva sua nota:"))
+if nota_aluno >= 90:
+    print("A")
+elif 80 <= nota_aluno <= 89:
+    print("B")
+elif 70 <= nota_aluno <= 79:
+    print("C")
+elif 60 <= nota_aluno <= 69:
+    print("D")
+else 0
+    print("F")
+    
