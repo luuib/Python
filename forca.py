23035b450d8602b43a649d927f92e6531bc40b3f
luuib <129198298+luuib@users.noreply.github.com>
2024-03-07 10:17:05 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -1,90 +1,26 @@
+import jogos
+secreta = "desculpa" 
+letraAcert = ["_","_","_","_","_","_","_","_"]
+tentativa = 7 
 def jogar():
-def jogar_forca():
-    print("Bem-vindo ao jogo da forca!")
-    palavra_secreta = obter_palavra_secreta()
-    letras_acertadas = ["_" for letra in palavra_secreta]
-    enforcou = False
-    acertou = False
-    erros = 0
-
-    print("A palavra é: ", " ".join(letras_acertadas))
-
-    while(not enforcou and not acertou):
-        chute = input("Qual letra? ").strip().lower()
-
-        if(chute in palavra_secreta):
+    while tentativa > 0 and  "_" in letraAcert:
+        palpite = input('digite uma letra').lower()
+        
+        if palpite in secreta:
             index = 0
-            for letra in palavra_secreta:
-                if(chute == letra):
-                    letras_acertadas[index] = letra
-                index += 1
+            for letra in secreta:
+                if palpite == letra:
+                    letraAcert [index] = letra
+                index += 1 
         else:
-            erros += 1
-            desenha_forca(erros)
-
-        enforcou = erros == 7
-        acertou = "_" not in letras_acertadas
-        print("A palavra é: ", " ".join(letras_acertadas))
-
-    if(acertou):
-        print("Parabéns, você ganhou!")
+            tentativa -= 1 
+            print(f'voce tem {tentativa} tentativas restantes')
+        print("_".join(letraAcert))
+    if "_" not in letraAcert:
+        print('parabens')
     else:
-        print("Você foi enforcado!")
-        print("A palavra era " + palavra_secreta)
+            print(f'voce perdeu, a palavra era {secreta}')
+    jogos.escolJogo()
 
-def obter_palavra_secreta():
-    palavras = ['casa', 'carro', 'bicicleta', 'computador', 'python','thales','paralelepipedo','carralho','kidbengala']
-    return random.choice(palavras)
-
-def desenha_forca(erros):
-    print("  _______     ")
-    print(" |/      |    ")
-
-    if(erros == 1):
-        print(" |      (_)   ")
-        print(" |            ")
-        print(" |            ")
-        print(" |            ")
-
-    if(erros == 2):
-        print(" |      (_)   ")
-        print(" |      \     ")
-        print(" |            ")
-        print(" |            ")
-
-    if(erros == 3):
-        print(" |      (_)   ")
-        print(" |      \|    ")
-        print(" |            ")
-        print(" |            ")
-
-    if(erros == 4):
-        print(" |      (_)   ")
-        print(" |      \|/   ")
-        print(" |            ")
-        print(" |            ")
-
-    if(erros == 5):
-        print(" |      (_)   ")
-        print(" |      \|/   ")
-        print(" |       |    ")
-        print(" |            ")
-
-    if(erros == 6):
-        print(" |      (_)   ")
-        print(" |      \|/   ")
-        print(" |       |    ")
-        print(" |      /     ")
-
-    if (erros == 7):
-        print(" |      (_)   ")
-        print(" |      \|/   ")
-        print(" |       |    ")
-        print(" |      / \   ")
-
-    print(" |            ")
-    print("_|___         ")
-    print()
-
-if __name__ == "__main__":
-    jogar_forca()
+if(__name__=="__main__"):
+    jogar()
