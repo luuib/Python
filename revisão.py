ef6a906bcf1918be43006917757dc5f610513366
luuib <129198298+luuib@users.noreply.github.com>
2024-04-18 12:12:16 -0300

    Update revisão.py

--- revisão.py
+++ revisão.py
@@ -1,33 +1,22 @@
 
-
-num1 = int(input("Escreva o primeiro numero"))
-num2 = int(input("Escreva o segundo numero"))
-
+num1 = int(input("Escreva o um numero"))
+num2 = int(input("Escreva o outro numero"))
 
 
-def escolher():
-    while True:
-        print("1-soma")
-        print("2-subtraçao")
-        print("3-divisao")
-        print("4-multiplicaçao")
-        print("5-sair")
+opc = input("qual calculo vc quer: +, -, /, *")
 
-        opc = input("qual calculo vc quer?")
+if opc == '+':
+    resultado = num1 + num2
+elif opc == '-':
+    resultado = num1 - num2
+elif opc == '/':        
+    resultado = num1 / num2
+elif opc == '*':
+    resultado = num1 * num2
 
-        if opc == '1':
-            resultado = num1 + num2
-        elif opc == '2':
-            resultado = num1 - num2
-        elif opc == '3':        
-            resultado = num1 / num2
-        elif opc == '4':
-            resultado = num1 * num2
-        elif opc == '5':
-            print("Adeus ...")
-            break
-        else:
-            print("È pra escolher uma dessas 5 opcoes")
 
-if __name__ == "__main__":
-    escolher()
+if num2 == 0:
+    print("Erro :nao tem como dividir")
+
+print(f"O resultado da operaçao e {num1} {opc} {num2} é: {resultado}")
+
